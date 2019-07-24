06dc3404af0879d74b2b65f41c338bb068bd4538
unknown <tranducquylhp@gmail.com>
2019-07-25 01:01:28 +0700

    tong duong cheo chinh v2

--- src/TongDuongCheoChinh.java
+++ src/TongDuongCheoChinh.java
@@ -1,14 +1,20 @@
+import java.util.Scanner;
+
 public class TongDuongCheoChinh {
     public static void main(String[] args) {
-        float[][] arr = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 16}
-        };
-        int height = arr.length;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap do dai cua mang: ");
+        int size = scanner.nextInt();
+        float[][] arr = new float[size][size];
+        for (int i=0; i<size; i++){
+            for (int j=0; j<size; j++){
+                System.out.println("Nhap phan tu hang "+i+" cot "+j+" = ");
+                arr[i][j] = scanner.nextFloat();
+            }
+        }
+
         float sum = 0;
-        for (int i = 0; i < height; i++) {
+        for (int i = 0; i < size; i++) {
             sum += arr[i][i];
         }
         System.out.println("Tong cac phan tu duong cheo chinh la: " + sum);
